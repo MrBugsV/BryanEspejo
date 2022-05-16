5fedd2e35056092e0422863f6af72e83e2c40025
Bryan <bryanespejo@hotmail.com>
2022-05-16 17:27:48 -0500

    Inicial

---
+++ app/src/main/java/fisei/uta/edu/ec/bryanespejo/Pantalla2.java
@@ -0,0 +1,14 @@
+package fisei.uta.edu.ec.bryanespejo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+    }
+}
